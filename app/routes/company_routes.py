5e2074d6260a3e687c7b99a522af6bb76c2f040c
Natalia Nunes <naticristinenunes@gmail.com>
2022-04-30 15:24:10 -0300

    adionado o get e readme de company

--- app/routes/company_routes.py
+++ app/routes/company_routes.py
@@ -3,7 +3,7 @@ from flask import Blueprint
 from app.controllers.company.signin_company_controller import register_company
 from app.controllers.company.update_company_controller import update_company
 from app.controllers.company.delete_company_controller import delete_company
-from app.controllers.company.get_company_controller import get_all_company
+from app.controllers.company.get_company_controller import get_all_company,get_one
 
 
 bp_company = Blueprint('bp_company', __name__,url_prefix='/company')
@@ -13,3 +13,4 @@ bp_company.put('')(update_company)
 bp_company.delete('')(delete_company)
 
 bp_company.get('')(get_all_company)
+bp_company.get('/<id>')(get_one)
