8353bd63fc44c3afabe206fc6484a1f051d165d7
Willian Santos <williansantosd2@gmail.com>
2022-05-01 16:33:09 -0300

    feat: added route to retrieve logged user data

--- app/controllers/user/retrieve_controllers.py
+++ app/controllers/user/retrieve_controllers.py
@@ -1,6 +1,10 @@
 from http import HTTPStatus
+from flask_jwt_extended import get_jwt_identity, jwt_required
+from sqlalchemy.orm import Query
 
 from app.models.user_model import UserModel, UserSchema
+from app.models.company_model import CompanyModel, CompanySchema
+
 
 
 def retrieve_users():
@@ -8,3 +12,22 @@ def retrieve_users():
     users = UserModel.query.all()
 
     return user_schema.dumps(users, many=True), HTTPStatus.OK
+
+@jwt_required()
+def retrieve_logged_user():
+    email = get_jwt_identity()
+    user_query: Query = UserModel.query
+    company_query: Query = CompanyModel.query
+
+    user_schema = UserSchema()
+    company_schema = CompanySchema()
+
+    user: UserModel = user_query.filter_by(email=email).first()
+    company: CompanyModel = company_query.filter_by(email=email).first()
+
+    if user:
+        return user_schema.dump(user), HTTPStatus.OK
+    if company:
+        return company_schema.dump(company), HTTPStatus.OK
+    
+    return '', HTTPStatus.NO_CONTENT
